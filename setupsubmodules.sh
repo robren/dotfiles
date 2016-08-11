774aaaeee2fa736ad51e16b9dfa0f3e1c3509661
Robert Rennison <robrennison@gmail.com>
2016-08-11 09:04:50 -0400

    remove plasticboy markdown from submodiles setup

--- setupsubmodules.sh
+++ setupsubmodules.sh
@@ -5,7 +5,7 @@ git submodule add git://git.wincent.com/command-t.git vim/bundle/command-t
 git submodule add https://github.com/vim-scripts/FuzzyFinder.git vim/bundle/FuzzyFinder
 git submodule add https://github.com/vim-scripts/abolish.vim.git vim/bundle/abolish.vim
 git submodule add https://github.com/scrooloose/nerdtree.git vim/bundle/nerdtree
-git submodule add https://github.com/plasticboy/vim-markdown.git vim/bundle/vim-markdown
+#git submodule add https://github.com/plasticboy/vim-markdown.git vim/bundle/vim-markdown
 git submodule add https://github.com/vim-scripts/L9.git vim/bundle/L9
 git submodule add http://github.com/ctrlpvim/ctrlp.vim vim/bundle/ctrlp.vim
 

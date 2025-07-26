3a6f8bdff11ce9dd917907eb323aebc7aac50f47
robren <rob@robren.net>
2025-07-26 16:51:16 -0400

    Added wrapper for Yazi can invoke with y and then will change directories when quit

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 
-# Put your new directory first, or at the end—either works:
 set -gx PATH ~/bin $PATH
 # └── new dir here ──┘  └── then append the existing PATH
+set -gx EDITOR nvim
 
 alias vim nvim
 

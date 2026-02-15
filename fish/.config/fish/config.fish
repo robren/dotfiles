1d7075954112ebdaea069dbf3f25a194688b3fca
robren <rob@robren.net>
2026-02-15 18:06:50 -0500

    Clean up fish config and add utility functions
    - Remove duplicate homebrew path (already set elsewhere)
    - Remove duplicate fzf source line
    - Set full path display (fish_prompt_pwd_dir_length 0)
    - Add utility functions: ls (eza wrapper), new, fcd, fdir
    - Configure FZF to use fd for faster file search
    - Clean up yazi cd wrapper function (y.fish)
    - Ignore tmux plugins directory

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,15 +1,17 @@
 
 set -gx PATH  ~/bin  /usr/local/bin $PATH
 # └── new dir here ──┘  └── then append the existing PATH
+
+#set -gx PATH /opt/homebrew/bin $PATH
 set -gx EDITOR nvim
 
 # not set by default on mac os
 set -x XDG_CONFIG_HOME "$HOME/.config"
 
 alias vim nvim
+fish_default_key_bindings
 
 # Set up fzf key bindings
-fzf --fish | source
 fzf --fish | source
 
 zoxide init fish | source
@@ -29,4 +31,35 @@ abbr -a gp  'git push'
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /Users/Rob/.cache/lm-studio/bin
 # End of LM Studio CLI section
+
+
+function ls
+    eza -l --sort=newest --reverse --group-directories-first --color=always $argv
+end
+
+function new --description 'List newest N entries with colors using eza'
+    set -l n 10
+    if test (count $argv) -ge 1
+        set n $argv[1]
+    end
+    eza -l --sort=modified --reverse --color=always | head -n $n
+end
+
+# using fd, a rust based faster find
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude Library --exclude .git --exclude node_modules'
+
+
+# cd to directory of selected file
+function fcd
+    set file (fzf)
+    and cd (dirname $file)
+end
+
+# cd to directory directly (searches only directories)
+function fdir
+    set dir (fd --type d --hidden --exclude Library | fzf)
+    and cd $dir
+end
+
+set fish_prompt_pwd_dir_length 0
 
--- fish/.config/fish/functions/y.fish
+++ fish/.config/fish/functions/y.fish
@@ -1,8 +1,8 @@
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if test -s "$tmp"
+        cd (cat "$tmp")
+    end
+    rm -f -- "$tmp"
 end

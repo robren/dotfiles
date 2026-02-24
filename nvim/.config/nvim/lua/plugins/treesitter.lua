54acd4a82771ba76585c7e89e9acf51c31c66f3a
Rob <Rob@Mac-Mini.local>
2026-02-23 18:50:16 -0800

    fix: remove stray kitty.conf from package root

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
+        require("nvim-treesitter.config").setup({
             ensure_installed = { "lua" }, -- Add any other languages you need
             highlight = { enable = true },
         })
